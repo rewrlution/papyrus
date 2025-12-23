e3d1818c83aac57c34a4d9e2dd199db6be297198
Huijing Huang <huijinghuang@microsoft.com>
2025-12-22 22:40:20 -0800

    start server; listen to port

---
+++ packages/api/src/index.ts
@@ -0,0 +1,8 @@
+import { createServer } from "./server.js";
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const server = createServer();
+
+server.listen(port, () => {
+  console.log(`API server running on http://localhost:${port}`);
+});
